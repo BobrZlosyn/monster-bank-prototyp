f70b00a41e12db1db01559106722c47f86e8c177
BobrZlosyn <KantorikM@seznam.cz>
2018-12-15 02:31:52 +0100

    dokončený prototyp

--- MonsterBank-Prototype/MonsterBank-Prototype/public_html/js/main.js
+++ MonsterBank-Prototype/MonsterBank-Prototype/public_html/js/main.js
@@ -1,5 +1,49 @@
 $().ready( function() { 
-console.log("ahoj");
-	$('#navbarSupportedContent').on('hide.bs.collapse', function (e) {
-	})
+	$("#login-pls").submit(function() {
+		let isOk = true;
+		if ($("#login").val().trim() == "") {		
+			$("#login-err").text("Vyplňte prosím přihlašovací jméno!");
+			redInput("#login", false);
+			isOk = false;
+		}else {
+			if ($("#login").val().trim().length < 5 ) {		
+				$("#login-err").text("Jméno musí mít aspoň pět znaků!");
+				isOk = false;
+				redInput("#login", false);
+			} else {
+				$("#login-err").text("");
+				redInput("#login", true);
+			}
+		}
+		
+		if ($("#pwd").val().trim() == "") {		
+			$("#pwd-err").text("Vyplňte prosím heslo!");
+			isOk = false;
+			redInput("#pwd", false);
+		} else {
+			if ($("#pwd").val().trim().length < 4 ) {		
+				$("#pwd-err").text("Heslo musí mít aspoň čtyři znaky!");
+				redInput("#pwd", false);
+				isOk = false;
+			} else {
+				$("#pwd-err").text("");
+				redInput("#pwd", true);
+			}			
+		}		
+		
+		return isOk;
+	});
 });
+
+function redInput(id, remove) {
+	
+	if (remove && $(id).hasClass("wrong-input")){
+		$(id).removeClass("wrong-input");
+	}else {
+		if (!remove) {
+			$(id).addClass("wrong-input");
+		}
+	}
+	
+}
+
